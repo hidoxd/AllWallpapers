711ab597906d78f0fa4249e7716dd56c3c29b56e
hidoxd <alex2108sk@gmail.com>
2026-08-31 23:54:33 +0500

    Delete MCMBridge.h

--- MCMBridge.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface AWMCMLease : NSObject
-@property(nonatomic, copy, readonly) NSString *rootPath;
-+ (instancetype)leaseForClass:(uint64_t)containerClass identifier:(NSString *)identifier error:(NSString **)error;
-- (BOOL)activate:(NSString **)error;
-@end
